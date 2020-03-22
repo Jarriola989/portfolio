cf0786b4bdecb7d8990c3ac540f37fb15b69ff6f
janeth <jarri023@fiu.edu>
2020-03-22 03:52:07 -0400

    mobile view adjustments

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8e112c26cf676dd369c094e652336ea0.js"
+  "/portfolio/precache-manifest.58815c873fb65a3aa268cdfc18168a44.js"
 );
 
 self.addEventListener('message', (event) => {
