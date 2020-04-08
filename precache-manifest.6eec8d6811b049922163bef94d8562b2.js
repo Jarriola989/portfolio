e7ec93efe590ffe7edb0fb4e5214ee21828ad903
janeth <jarri023@fiu.edu>
2020-04-08 15:28:52 -0400

    Updates

---
+++ precache-manifest.6eec8d6811b049922163bef94d8562b2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6460f5b87b6b152d08733257c101cbb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "54b479ee298c43b2fd7d",
+    "url": "/portfolio/static/css/main.6d54906e.chunk.css"
+  },
+  {
+    "revision": "352e3f2b8506cfa12a0b",
+    "url": "/portfolio/static/js/2.1015a431.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.1015a431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b479ee298c43b2fd7d",
+    "url": "/portfolio/static/js/main.dd13441e.chunk.js"
+  },
+  {
+    "revision": "11351ff706ca9640736e",
+    "url": "/portfolio/static/js/runtime-main.3b971105.js"
+  },
+  {
+    "revision": "2b63970bac586f3ae723c52b53facef9",
+    "url": "/portfolio/static/media/Java.2b63970b.jpg"
+  },
+  {
+    "revision": "894bef2fffd5fea517c49cd1fc091df1",
+    "url": "/portfolio/static/media/MiamiHacks.894bef2f.png"
+  },
+  {
+    "revision": "5cf39d01d5bd7312cc9eb68cff587e2f",
+    "url": "/portfolio/static/media/computer.5cf39d01.jpeg"
+  },
+  {
+    "revision": "d4e64920e4c9f824818d7830b559d308",
+    "url": "/portfolio/static/media/cplusplus.d4e64920.jpg"
+  },
+  {
+    "revision": "029a7f7919731b550b735d7ed6cd305c",
+    "url": "/portfolio/static/media/geektext.029a7f79.png"
+  },
+  {
+    "revision": "398b7a11ff9b94157d806ab2d3fd103d",
+    "url": "/portfolio/static/media/graphql.398b7a11.png"
+  },
+  {
+    "revision": "e87e26c1f79051d2a615218fbeaec6a2",
+    "url": "/portfolio/static/media/html-css.e87e26c1.jpg"
+  },
+  {
+    "revision": "0c6dab7f81aad00b3560118efa720ee2",
+    "url": "/portfolio/static/media/js-ts.0c6dab7f.png"
+  },
+  {
+    "revision": "7e47a6350869e3441ebec7c132a8f797",
+    "url": "/portfolio/static/media/node.7e47a635.jpg"
+  },
+  {
+    "revision": "46996aca606038a8e03a701fa34dfcc3",
+    "url": "/portfolio/static/media/orange-cloud.46996aca.jpg"
+  },
+  {
+    "revision": "abbe688d224ec54b91f44565e6472a95",
+    "url": "/portfolio/static/media/prisma.abbe688d.png"
+  },
+  {
+    "revision": "48eebb109cf849623be361cdba6a48eb",
+    "url": "/portfolio/static/media/react.48eebb10.png"
+  },
+  {
+    "revision": "451c44a71db16615c34edbdd15b7de1c",
+    "url": "/portfolio/static/media/redis.451c44a7.jpg"
+  },
+  {
+    "revision": "c52d202bdbec8e3cc6356eb0b5a677f0",
+    "url": "/portfolio/static/media/rhaz.c52d202b.png"
+  },
+  {
+    "revision": "325d3433048e5d94622798617bb49833",
+    "url": "/portfolio/static/media/springboot.325d3433.png"
+  },
+  {
+    "revision": "07d7674110a05372c05e56e669ccf06a",
+    "url": "/portfolio/static/media/wise-code.07d76741.png"
+  },
+  {
+    "revision": "ba0979f5ed123e6b9b3fefaf5e557fc5",
+    "url": "/portfolio/static/media/wisedb.ba0979f5.png"
+  }
+]);
